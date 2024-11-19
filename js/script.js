0e1a8899df0489814d4566cc90bf7213e40b08fa
kayky <kayky.lucas.quark@gmail.com>
2024-11-19 09:50:25 -0300

    aula 19/11

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ function mostrarDescricao(id){
     const texto = document.getElementById(id)
     if (texto.style.display == "none") {
         texto.style.display = "block"
+        console.log(texto.style.display);
         
     }else{
         texto.style.display =   "none"
+        console.log(texto.style.display);
     }
 }
